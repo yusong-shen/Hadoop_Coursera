74fda7b1a469b75f0b171e73973f815b32add3e5
yusong-shen <yusongtju@gmail.com>
2016-03-17 17:29:24 -0500

    implemment mapper function

--- A2_JoinData/join2_mapper.py
+++ A2_JoinData/join2_mapper.py
@@ -2,14 +2,15 @@
 import sys
 
 # --------------------------------------------------------------------------
-#This mapper code will input a <date word, value> input file, and move date into 
-#  the value field for output
-#  
-#  Note, this program is written in a simple style and does not full advantage of Python 
-#     data structures,but I believe it is more readable
-#
-#  Note, there is NO error checking of the input, it is assumed to be correct
-#     meaning no extra spaces, missing inputs or counts,etc..
+# <TV show, count>
+# e.g Almost_News, 25
+# a possible pseudo code example of how to implement this join using tv-show as the key:
+
+# join2_mapper:
+
+# read lines, and split lines into key & value
+
+# if value is ABC or if value is a digit print it out
 #
 # See #  see https://docs.python.org/2/tutorial/index.html for details  and python  tutorials
 #
@@ -20,17 +21,14 @@ import sys
 for line in sys.stdin:
     line       = line.strip()   #strip out carriage return
     key_value  = line.split(",")   #split line, into key and value, returns a list
-    key_in     = key_value[0].split(" ")   #key is first item in list
+    key_in     = key_value[0]   #key is first item in list
     value_in   = key_value[1]   #value is 2nd item 
 
-    #print key_in
-    if len(key_in)>=2:           #if this entry has <date word> in key
-        date = key_in[0]      #now get date from key field
-        word = key_in[1]
-        value_out = date+" "+value_in     #concatenate date, blank, and value_in
-        print( '%s\t%s' % (word, value_out) )  #print a string, tab, and string
-    else:   #key is only <word> so just pass it through
-        print( '%s\t%s' % (key_in[0], value_in) )  #print a string tab and string
+
+    if value_in == "ABC" or value_in.isdigit() :
+
+        print('%s\t%s' % (key_in, value_in))
+
 
 #Note that Hadoop expects a tab to separate key value
 #but this program assumes the input file has a ',' separating key value
