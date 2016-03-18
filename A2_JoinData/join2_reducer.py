7b876d48e855f1131090e8270ac5d7f99e7a2906
yusong-shen <yusongtju@gmail.com>
2016-03-17 21:04:02 -0500

    implement the reducer function

--- A2_JoinData/join2_reducer.py
+++ A2_JoinData/join2_reducer.py
@@ -23,7 +23,7 @@ import sys
 
 last_key = None
 running_total = 0
-abc_found = false 
+abc_found = False 
 
 line_cnt           = 0  #count input lines
 
@@ -32,6 +32,44 @@ for line in sys.stdin:
     key_value  = line.split('\t')   #split line, into key and value, returns a list
     line_cnt   = line_cnt+1     
 
+    # e.g  (Almost_News, ABC)
+    #       (Almost_News, 35)
     #note: for simple debugging use print statements, ie:  
-    key_in  = key_value[0]         #key is first item in list, indexed by 0
+    curr_key  = key_value[0]         #key is first item in list, indexed by 0
     value_in   = key_value[1]         #value is 2nd item
+
+    # check if key has changed
+    # if not, add value to running total
+    if curr_key == last_key :
+        # if value is ABC then set some variable to mark that ABC was found 
+        if value_in == "ABC" :
+            abc_found = True;
+        else :
+            running_total += int(value_in)
+    else :
+    # a key has changed (and it's not the first input) 
+
+        # check if it's not the first input
+        if line_cnt > 1:
+            # check if ABC had been found
+            if abc_found :
+                # print out the last key and running total
+                print( "{0}\t{1}".format(last_key, running_total) )
+                # reset "ABC" to not found
+                abc_found = False
+
+        # reset values
+        if value_in == "ABC" :
+            abc_found = True;
+        else :
+            running_total = int(value_in)
+
+        last_key = curr_key
+
+
+# ---------------------------------------------------------------
+#now write out the LAST join result
+# ---------------------------------------------------------------
+
+if last_key == curr_key and abc_found:
+    print( "{0}\t{1}".format(last_key, running_total))
