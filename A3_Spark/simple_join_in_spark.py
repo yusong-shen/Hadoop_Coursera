779f2fbc910daf3cb699ee211752ad1e1013454a
yusong-shen <yusongtju@gmail.com>
2016-03-20 22:08:00 -0500

    init commit for A3 Spark lesson 2

---
+++ A3_Spark/simple_join_in_spark.py
@@ -0,0 +1,20 @@
+
+# Mapper for fileA
+
+
+def split_fileA(line):
+    # split the input line in word and count on the comma
+    # <ENTER_CODE_HERE>
+    # turn the count to an integer  
+    # <ENTER_CODE_HERE>
+    return (word, count)
+
+
+
+# Mapper for fileB
+
+def split_fileB(line):
+    # split the input line into word, date and count_string
+    # <ENTER_CODE_HERE>
+    # <ENTER_CODE_HERE>
+    return (word, date + " " + count_string) 
