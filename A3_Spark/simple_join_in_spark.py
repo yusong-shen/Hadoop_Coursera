1ba22e8d71b24ab9c1abb0e01d112d3a90e13086
yusong-shen <yusongtju@gmail.com>
2016-03-21 22:10:48 -0500

    Implement fileB  mapper function for A3 simple join in spark

--- A3_Spark/simple_join_in_spark.py
+++ A3_Spark/simple_join_in_spark.py
@@ -6,12 +6,10 @@ def split_fileA(line):
     # split the input line in word and count on the comma
     line       = line.strip()   #strip out carriage return
     key_value  = line.split(",")   #split line, into key and value, returns a list
-
     # turn the count to an integer  
     word     = key_value[0]   #key is first item in list
     value_in   = key_value[1]   #value is 2nd item 
     count =  int(value_in)
-
     return (word, count)
 
 
@@ -22,18 +20,19 @@ def split_fileB(line):
     # split the input line into word, date and count_string
     line       = line.strip()   #strip out carriage return
     key_value  = line.split(",")   #split line, into key and value, returns a list
-
-    word = key_value[0]
-    date = key_value[1]
-    count_string = key_value[2]
-
+    key_in     = key_value[0].split(" ")   #key is first item in list
+    value_in   = key_value[1]   #value is 2nd item 
+    word = key_in[1]
+    date = key_in[0]
+    count_string = value_in
     return (word, date + " " + count_string) 
-    pass
+
+
 
 
 # Test
 # test_line = "able,991"
 # print split_fileA(test_line)
 
-# test_line = "able,Jan-01,5"
-# print split_fileB(test_line)
+test_line = "Dec-15 able,100"
+print split_fileB(test_line)
